87341211b77c530c16be3705b7fad19fc34d4b0d
Шаукат <shaukat.off@mail.ru>
2022-12-12 01:39:23 +0300

    какой то код есть

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿/*Задача: 
+Написать программу, которая из имеющегося массива строк
+ формирует новый массив из строк, 
+ длина которых меньше, либо равна 3 символам.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []*/
+
+
+void methodArray (string [] array)
+{
+     string[] newarr = new string[array.Length];
+    
+     for (int i=0; i<array.Length; i++)
+        {      
+            if(array[i].Length<=3)
+            {
+                newarr[i]=array[i]+",";
+                Console.Write(newarr[i]); 
+            }
+        } 
+}
+        
+
+string [] arr = {"1234", "156", "-2", "computer science"};
+
+methodArray(arr);
